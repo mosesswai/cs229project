8f61c185dbfc6229f2e39784d9e49ea5c1e81b30
Kirk Ampong <kirkampong2@gmail.com>
2020-10-23 16:42:21 -0700

    Minor update

--- logistic_regression.py
+++ logistic_regression.py
@@ -15,80 +15,80 @@ sns.set(style="white")
 sns.set(style="whitegrid", color_codes=True)
 
 #import the data
-df = pd.read_csv('african_crises.csv', index_col = 0) #index col neccessary?
-df.head()
+dataframe = pd.read_csv('african_crises.csv', index_col = 0) #index col neccessary?
+dataframe.head()
 
-print(df.shape) #(1059, 13)
+print(dataframe.shape) #(1059, 13)
 
 
 # Plot total number of cases by country
-sns.countplot(x='country', data=df, palette ='hls')
+sns.countplot(x='country', data=dataframe, palette ='hls')
 plt.xticks(rotation=90)
 plt.show()
 
 # Plot counts of 'x'_crisis, where 'x' can equals: systemic, currency, inflation, branking.
-sns.countplot(x='systemic_crisis', data=df, palette ='hls')
+sns.countplot(x='systemic_crisis', data=dataframe, palette ='hls')
 plt.xticks(rotation=90)
 plt.show()
-print(df['banking_crisis'].value_counts()) #print counts
+print(dataframe['banking_crisis'].value_counts()) #print counts
 
 #------------------------------------------------------------------------------------------
 #						         	PRE-PROCESSING
 #------------------------------------------------------------------------------------------
 
 # Drop the categorical variables which are not useful to the dataset for logistic regression
-df = df.drop(['cc3', 'country', 'year'], axis =1)
-df.head()
+dataframe = dataframe.drop(['cc3', 'country', 'year'], axis =1)
+dataframe.head()
 
 # drop this column since it is not informative (jsutify with plot later)
-df = df.drop(['gdp_weighted_default'], axis =1)
-df.head()
+dataframe = dataframe.drop(['gdp_weighted_default'], axis =1)
+dataframe.head()
 
 # Define ouput column y and drop from dataset
-y = df[['banking_crisis']]
-y = pd.get_dummies(df['banking_crisis'],drop_first=True)
-df = df.drop(['banking_crisis'], axis =1)
-df.head()
+y = dataframe[['banking_crisis']]
+y = pd.get_dummies(dataframe['banking_crisis'],drop_first=True)
+dataframe = dataframe.drop(['banking_crisis'], axis =1)
+dataframe.head()
 
 
 #------------------------------------------------------------------------------------------
-#						         	  TRAINING
+#					  TRAINING
 #------------------------------------------------------------------------------------------
 
 # split the data into test train sets
 from sklearn.model_selection import train_test_split
 # create training and testing vars
-X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2)
-print(X_train.shape, y_train.shape)
-print(X_test.shape, y_test.shape)
-# fitting
+X_train, X_test, Y_train, Y_test = train_test_split(dataframe, y, test_size=0.2)
+print(X_train.shape, Y_train.shape)
+print(X_test.shape, Y_test.shape)
+# train
 from sklearn.linear_model import LogisticRegression
 logmodel = LogisticRegression()
-logmodel.fit(X_train, y_train)
-#predictions
+logmodel.fit(X_train, Y_train)
+# predict
 Predictions = logmodel.predict(X_test)
 
 
 #------------------------------------------------------------------------------------------
-#						         	  REPORT
+#						REPORT
 #------------------------------------------------------------------------------------------
 from sklearn.metrics import classification_report
-print(classification_report(y_test,Predictions))
+print(classification_report(Y_test,Predictions))
 # confusion matrix
 from sklearn.metrics import confusion_matrix
-print(confusion_matrix(y_test, Predictions))
+print(confusion_matrix(Y_test, Predictions))
 
 # calculate the fpr and tpr for all thresholds of the classification
 import sklearn.metrics as metrics
 probs = logmodel.predict_proba(X_test)
 preds = probs[:,1]
-fpr, tpr, threshold = metrics.roc_curve(y_test, preds)
+fpr, tpr, threshold = metrics.roc_curve(Y_test, preds)
 roc_auc = metrics.auc(fpr, tpr)
 
 
 
 #------------------------------------------------------------------------------------------
-#						         	  DISPLAY
+#						DISPLAY
 #------------------------------------------------------------------------------------------
 
 
